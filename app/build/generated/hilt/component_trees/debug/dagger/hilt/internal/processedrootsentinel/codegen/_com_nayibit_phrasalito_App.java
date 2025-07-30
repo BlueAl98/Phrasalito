398670dbd0a499af03e51eb46ebf2db4242ed350
Najib Alejandro <alejandrorod1498@gmail.com>
2025-07-30 13:47:11 -0600

    clean project

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_nayibit_phrasalito_App.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.nayibit.phrasalito.App"
-)
-public final class _com_nayibit_phrasalito_App {
-}
