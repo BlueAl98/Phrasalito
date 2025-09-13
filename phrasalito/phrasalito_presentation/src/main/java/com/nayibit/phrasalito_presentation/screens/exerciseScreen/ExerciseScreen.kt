a5fc2cf59f60c0a5d8b4f96142d8608236be8b77
Najib Alejandro <alejandrorod1498@gmail.com>
2025-09-13 16:15:12 -0600

    Update UI BottomSheet
    The user interface of the BottomSheet has been updated, including the addition of new string resources. The layout has been modified to incorporate a CircularProgressIndicator, labels, and a "Finish Exercise" button, improving the visual presentation and user interaction.

--- phrasalito/phrasalito_presentation/src/main/java/com/nayibit/phrasalito_presentation/screens/exerciseScreen/ExerciseScreen.kt
+++ phrasalito/phrasalito_presentation/src/main/java/com/nayibit/phrasalito_presentation/screens/exerciseScreen/ExerciseScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -25,6 +26,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
@@ -37,6 +39,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.nayibit.phrasalito_presentation.R
@@ -108,18 +112,12 @@ fun ExerciseScreen(
         snackbarHost = { SnackbarHost(snackbarHostState) },
         sheetContent = {
 
-            BottomSheetContent(
-                state = state,
-                onEvent = onEvent
-            )
-
-                Button(onClick = {
-                    coroutineScope.launch {
-                        sheetState.bottomSheetState.partialExpand()
-                    }
-                }) {
-                    Text("Collapse")
-                }
+            Column (modifier.fillMaxHeight(0.5f)){
+                BottomSheetContent(
+                    state = state,
+                    onEvent = onEvent
+                )
+            }
 
         }
     ) { padding ->
@@ -287,18 +285,39 @@ fun BottomSheetContent(
     state: ExerciseUiState,
     onEvent: (ExerciseUiEvent) -> Unit
 ){
-        Column(modifier.fillMaxSize()) {
-
-            Box (modifier.weight(1f)
-                .fillMaxWidth(), contentAlignment = Alignment.Center){
+        Column(modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+            ) {
 
                 CircularProgressIndicator(
                     progress = state.testProgressCorrectAnswers,
+                    strokeWidth = 20.dp,
+                    size = 150.dp
                 )
+               Spacer(modifier = Modifier.height(20.dp))
+
+                    Text(stringResource(R.string.label_percentage_correct_answers), modifier = Modifier.padding(horizontal = 16.dp).fillMaxWidth(),
+                        textAlign = TextAlign.Center,
+                        fontWeight = FontWeight.Bold,
+                        style = MaterialTheme.typography.titleLarge,
+                        color = Color.Red)
+
+               Spacer(modifier = Modifier.height(16.dp))
+
+            Text(stringResource(R.string.label_description_percentage_correct_answers), modifier = Modifier.padding(horizontal = 16.dp).fillMaxWidth(),
+                        textAlign = TextAlign.Center,
+                        style = MaterialTheme.typography.labelMedium,
+                        color = Color.Gray
+                        )
+             Spacer(modifier = Modifier.height(16.dp))
+
+                    ButtonBase(
+                        onClick = { onEvent(ExerciseUiEvent.OnStartClicked) },
+                        text = stringResource(R.string.btn_finish_exercise),
+                        modifier = Modifier.padding(horizontal = 16.dp)
+                    )
+
+                }
+
             }
-
-
-        }
-
-
-}
