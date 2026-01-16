38c804eb7386c7a532ae0dee777ae437792e1f3f
Najib Alejandro <alejandrorod1498@gmail.com>
2026-01-16 17:33:25 -0600

    update version 1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.nayibit.phrasalito"
         minSdk = 29
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
