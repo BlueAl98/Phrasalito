788f1d166c62c4bbed8148ab90dc5a8f13bd6aec
Najib Alejandro <alejandrorod1498@gmail.com>
2026-01-30 12:48:54 -0600

    clean

--- phrasalito/phrasalito_presentation/src/main/java/com/nayibit/phrasalito_presentation/composables/LinearProgressExpresive.kt
+++ phrasalito/phrasalito_presentation/src/main/java/com/nayibit/phrasalito_presentation/composables/LinearProgressExpresive.kt
@@ -4,17 +4,11 @@ import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.requiredHeight
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.LinearWavyProgressIndicator
 import androidx.compose.material3.ProgressIndicatorDefaults
-import androidx.compose.material3.Slider
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableFloatStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
