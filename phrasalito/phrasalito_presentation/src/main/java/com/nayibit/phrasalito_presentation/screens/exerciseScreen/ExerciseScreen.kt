a8bfc49b0d0d95cc13eaf13c38ed3d8cf8f73b19
Najib Alejandro <alejandrorod1498@gmail.com>
2026-01-31 12:20:04 -0600

    refactor: Initialize bottom sheet state to hidden
    The bottom sheet on the exercise screen is now initialized in a hidden state and will only expand when `state.isBottomSheetExpanded` is true. The logic to partially expand it in the `else` case has been removed.

--- phrasalito/phrasalito_presentation/src/main/java/com/nayibit/phrasalito_presentation/screens/exerciseScreen/ExerciseScreen.kt
+++ phrasalito/phrasalito_presentation/src/main/java/com/nayibit/phrasalito_presentation/screens/exerciseScreen/ExerciseScreen.kt
@@ -27,10 +27,12 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SheetValue
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberBottomSheetScaffoldState
+import androidx.compose.material3.rememberStandardBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
@@ -65,15 +67,18 @@ fun ExerciseScreen(
     navigation: () -> Unit
 ) {
     val snackbarHostState = remember { SnackbarHostState() }
-    val sheetState = rememberBottomSheetScaffoldState()
+    val sheetState = rememberBottomSheetScaffoldState(
+        bottomSheetState = rememberStandardBottomSheetState(
+            initialValue = SheetValue.Hidden,
+            skipHiddenState = false
+        )
+    )
     val orientation = LocalConfiguration.current.orientation
 
 
     LaunchedEffect(state.isBottomSheetExpanded) {
         if (state.isBottomSheetExpanded) {
             sheetState.bottomSheetState.expand()
-        } else {
-            sheetState.bottomSheetState.partialExpand()
         }
     }
 
